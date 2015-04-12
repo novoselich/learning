10b4760605f687cabc91d8cc51ec7d4c636caae0
novoselich <oleg.n@mail.ua>
2015-04-12 22:43:51 +0300

    implement boundService

--- Services/app/src/main/java/ua/grt/services/boundservice/BoundServiceFragment.java
+++ Services/app/src/main/java/ua/grt/services/boundservice/BoundServiceFragment.java
@@ -2,21 +2,29 @@ package ua.grt.services.boundservice;
 
 import android.app.Activity;
 import android.app.Fragment;
+import android.content.BroadcastReceiver;
+import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.ServiceConnection;
 import android.os.Bundle;
+import android.os.IBinder;
 import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ProgressBar;
+import android.widget.RelativeLayout;
 import android.widget.SeekBar;
 import android.widget.TextView;
 
 import ua.grt.services.R;
 
-public class BoundServiceFragment extends Fragment implements View.OnClickListener {
+public class BoundServiceFragment extends Fragment implements View.OnClickListener, SeekBar.OnSeekBarChangeListener {
+
+    private static final int DEFAULT_INDICATOR_SIZE = 30;
 
     private Context context;
     private boolean started;
@@ -29,8 +37,81 @@ public class BoundServiceFragment extends Fragment implements View.OnClickListen
     private ProgressBar mProgressBar;
 
     private Intent mServiceIntent;
+    private MyBoundService.MyBinder service;
 
-    // TODO define the receiver
+    private ServiceConnection mServiceConnection = new ServiceConnection() {
+
+        @Override
+        public void onServiceConnected(ComponentName componentName, IBinder iBinder) {
+            bound = true;
+            mBindBtn.setText(R.string.unbind_btn_caption);
+            updateStatus();
+            updateIndicator();
+            service = (MyBoundService.MyBinder) iBinder;
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName componentName) {
+            bound = false;
+            mBindBtn.setText(R.string.bind_btn_caption);
+            updateStatus();
+            updateIndicator();
+            service = null;
+        }
+    };
+
+    private BroadcastReceiver receiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            Bundle bundle = intent.getExtras();
+            if (bundle != null) {
+                if (bundle.containsKey(MyBoundService.WORKING)) {
+                    started = bundle.getBoolean(MyBoundService.WORKING);
+                    if (!started) {
+                        mStartBtn.setText(R.string.start_btn_caption);
+                    } else {
+                        mStartBtn.setText(R.string.stop_btn_caption);
+                    }
+                    updateStatus();
+                }
+                updateIndicator(bundle.getInt(MyBoundService.VALUE, DEFAULT_INDICATOR_SIZE));
+            }
+        }
+    };
+
+    private void updateStatus() {
+        if (!started && !bound) {
+            mStatusText.setText("");
+            return;
+        }
+        if (started && !bound) {
+            mStatusText.setText(R.string.started_msg);
+            return;
+        }
+        if (!started && bound) {
+            mStatusText.setText(R.string.bound_msg);
+            return;
+        }
+        if (started && bound) {
+            mStatusText.setText(R.string.started_and_bound_msg);
+            return;
+        }
+    }
+
+    private void updateIndicator() {
+        updateIndicator(DEFAULT_INDICATOR_SIZE);
+    }
+
+    private void updateIndicator(int size) {
+        if (!started && !bound) {
+            mProgressBar.setVisibility(View.GONE);
+            return;
+        }
+        mProgressBar.setVisibility(View.VISIBLE);
+        mProgressBar.setLayoutParams(new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.MATCH_PARENT, size));
+
+    }
+
 
     @Override
     public void onAttach(Activity activity) {
@@ -43,7 +124,7 @@ public class BoundServiceFragment extends Fragment implements View.OnClickListen
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.bound_service_layout, null, false);
 
-        // TODO create an intent
+        mServiceIntent = new Intent(context, MyBoundService.class);
 
         mStartBtn = (Button) view.findViewById(R.id.start_stop_boundService_btn);
         mBindBtn = (Button) view.findViewById(R.id.bind_unbind_boundService_btn);
@@ -53,25 +134,59 @@ public class BoundServiceFragment extends Fragment implements View.OnClickListen
 
         mBindBtn.setOnClickListener(this);
         mStartBtn.setOnClickListener(this);
+        mIndicatorSize.setOnSeekBarChangeListener(this);
 
         return view;
     }
 
-    // TODO register & unregister receiver
-
-
     @Override
-    public void onStart() {
-        super.onStart();
+    public void onResume() {
+        super.onResume();
+        context.registerReceiver(receiver, new IntentFilter(MyBoundService.NOTIFICATION));
     }
 
     @Override
     public void onPause() {
         super.onPause();
+        context.unregisterReceiver(receiver);
     }
+
 
     @Override
     public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.start_stop_boundService_btn:
+                if (!started) {
+                    context.startService(mServiceIntent);
+                } else {
+                    context.stopService(mServiceIntent);
+                }
+                break;
+            case R.id.bind_unbind_boundService_btn:
+                if (!bound) {
+                    context.bindService(mServiceIntent, mServiceConnection, Context.BIND_AUTO_CREATE);
+                } else {
+                    context.unbindService(mServiceConnection);
+                    bound = false;
+                    mBindBtn.setText(R.string.bind_btn_caption);
+                    updateStatus();
+                    updateIndicator();
+                }
+        }
+    }
 
+    @Override
+    public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+        if (bound && service != null) {
+            service.setValue(i);
+        }
+    }
+
+    @Override
+    public void onStartTrackingTouch(SeekBar seekBar) {
+    }
+
+    @Override
+    public void onStopTrackingTouch(SeekBar seekBar) {
     }
 }
---
+++ Services/app/src/main/java/ua/grt/services/boundservice/MyBoundService.java
@@ -0,0 +1,44 @@
+package ua.grt.services.boundservice;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.Binder;
+import android.os.IBinder;
+
+public class MyBoundService extends Service {
+
+    static final String NOTIFICATION = "ua.grt.services.boundservice";
+    static final String WORKING = "working";
+    static final String VALUE = "value";
+
+    class MyBinder extends Binder {
+        public void setValue(int value) {
+            Intent intent = new Intent(NOTIFICATION);
+            intent.putExtra(VALUE, value);
+            sendBroadcast(intent);
+        }
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        publish(true);
+        return START_NOT_STICKY;
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        return new MyBinder();
+    }
+
+    @Override
+    public void onDestroy() {
+        publish(false);
+        super.onDestroy();
+    }
+
+    private void publish(boolean working) {
+        Intent intent = new Intent(NOTIFICATION);
+        intent.putExtra(WORKING, working);
+        sendBroadcast(intent);
+    }
+}
