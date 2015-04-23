5ac63bb3c75af93e77d3794dc01f2cccf9bf4d83
novoselich <oleg.n@mail.ua>
2015-04-23 21:09:00 +0300

    run intentservice in separate process

--- Services/app/src/main/java/ua/grt/services/startedservice/MyStartedService.java
+++ Services/app/src/main/java/ua/grt/services/startedservice/MyStartedService.java
@@ -33,8 +33,6 @@ public class MyStartedService extends Service {
             quit = true;
         }
 
-        ;
-
         @Override
         public void handleMessage(Message msg) {
             quit = false;
